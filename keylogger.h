1ee421492a607f9764a9ddda21bcffdaf80f6612
Fabio Cinicolo <f.cinicolo@studenti.unina.it>
2023-08-05 14:34:25 +0200

    Update keylogger.h

--- keylogger.h
+++ keylogger.h
@@ -13,6 +13,7 @@ int writeEventsIntoFile(int fd, struct input_event *events, size_t to_write);
 int keyboardFound(char *path, int *keyboard_fd);
 int hasRelativeMovement(int fd);
 int hasAbsoluteMovement(int fd);
+int hasEventTypes(int fd, unsigned long evbit_to_check);
 int hasKeys(int fd);
 int hasSpecificKeys(int fd, int *keys, size_t num_keys);
 void sigHandler(int signum);
